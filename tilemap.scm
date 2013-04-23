11994ec70b3ecf01783d558545c239015f00ce39
Magnus <yotomten@yotomten-eME728.(none)>
2013-04-23 14:04:45 +0200

    Lade till en tilemap.scm

---
+++ tilemap.scm
@@ -0,0 +1,17 @@
+(define tile-map%
+  (class object%
+    (init w
+          h)
+    (define width w)
+    (define height h)
+    
+    (define tiles (make-vector (* width height) #f))
+    
+    (define (tile x y)
+      (+ (* width y) x))
+    
+    (define/public (get-tile x y)
+      (vector-ref tiles (tile x y)))
+    
+    (define/public (set-tile! x y value)
+      (vector-set! tiles (tile x y) value))))
