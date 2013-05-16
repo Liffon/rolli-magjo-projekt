5092db635fbe8073b83a31c18ca2395264d4ec9d
Rolf Lifvergren <liffon@gmail.com>
2013-05-16 10:47:40 +0200

    Ändrade till hjärtan för hp-visning

--- hud.scm
+++ hud.scm
@@ -3,17 +3,22 @@
     (init-field
      player)
     (define game-over-bitmap (read-bitmap "sprites/game-over2.png"))
+    (define empty-heart-bitmap (read-bitmap "sprites/empty-heart.png"))
+    (define heart-bitmap (read-bitmap "sprites/heart.png"))
     
     (define/public (hit-points life dc)
       (for-each (λ (x)
-                  (send dc set-brush "white" 'solid)
-                  (send dc draw-ellipse x 10 10 10))
+                  ;(send dc set-brush "white" 'solid)
+                  ;(send dc draw-ellipse x 10 10 10)
+                  (send dc draw-bitmap empty-heart-bitmap x 10))
                 (range 10 (+ (* 10 10) 10) 20))
       (for-each (λ (hit-point)
-                  (send dc set-brush "red" 'solid)
-                  (send dc draw-ellipse hit-point 10 10 10))
+                  ;(send dc set-brush "red" 'solid)
+                  ;(send dc draw-ellipse hit-point 10 10 10)
+                  (send dc draw-bitmap heart-bitmap hit-point 10))
                 (range 10 (+ (* life 10) 10) 20)))
     
+    ;; Ritar ut vapnets bild (om den finns) och namn
     (define/public (draw-weapon canvas dc)
       (let ([weapon (get-field weapon player)])
         (when weapon
@@ -24,7 +29,7 @@
                             0)])
             (when bitmap
               (send dc draw-bitmap bitmap 10 30))
-            (send dc draw-text name (+ 10 width) 30)))))
+            (send dc draw-text name (+ 10 width) 30 #t)))))
     
     (define/public (game-over dc)
       (when (zero? (get-field hp player))
