48a0f99d4bf3ad6ad62a87be77ff14c49ca3a77c
Magnus <yotomten@yotomten-eME728.(none)>
2013-05-23 20:04:56 +0200

    kommenterat bullet%

--- bullet.scm
+++ bullet.scm
@@ -8,20 +8,23 @@
                 [direction #f]
                 [speed 1]
                 [the-map #f])
-
+    
+    ;;tar bort kulan från banan
+    
     (define/public (remove-self!)
       (when the-map
         (send the-map delete-element! this)
         (set! the-map #f)))
     
+    ;; tar in canvas och dc som argument och ritar kulan på banan
     (define/public (render canvas dc)
       (send the-map draw-rectangle x y width height "black" canvas dc))
     
+    ;;flyttar kulan 
     (define/public (move!)
       (set! x ((if (eq? direction 'left) - +) ;; flytta åt rätt håll beroende på riktning
                x
                (* speed *dt*))))
-    
     (define/public (update!)
       (move!))
     (super-new)))
